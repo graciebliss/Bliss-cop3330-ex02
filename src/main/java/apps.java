95e9a4342be2e82135b745404108ccbc6d165b3e
graciebliss <gracieabliss@gmail.com>
2021-06-06 13:58:39 -0400

    final

--- src/main/java/apps.java
+++ src/main/java/apps.java
@@ -1,2 +1,17 @@
+
+/*
+ *  UCF COP3330 Summer 2021 Assignment 1 Solution
+ *  Copyright 2021 Gracie Bliss
+ */
+
+import java.util.*;
 public class apps {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("What is the input string? ");
+        String input=in.nextLine();
+        int length= input.length();
+        System.out.println(input+" has "+length+" characters.");
+        in.close();
+    }
 }
